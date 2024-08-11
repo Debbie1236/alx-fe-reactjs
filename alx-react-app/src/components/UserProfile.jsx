71c171daa795ea89279a13270dd23ce3e2c19b6b
Debbie1236 <debbieodhiambo91@gmail.com>
2024-08-11 16:35:43 +0300

    Update UserProfile.jsx

--- alx-react-app/src/components/UserProfile.jsx
+++ alx-react-app/src/components/UserProfile.jsx
@@ -1,7 +1,9 @@
-export default function MainContent() {
+export default function UserProfile(props) {
   return (
-    <main>
-      <p>I love to visit New York, Paris, and Tokyo.</p>
-    </main>
+    <div>
+      <h2>{props.name}</h2>
+      <p>Age: {props.age}</p>
+      <p>Bio: {props.bio}</p>
+    </div>
   );
 }
