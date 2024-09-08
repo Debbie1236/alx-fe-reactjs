301d73f30c5d845c078c9e01626a18cace2b9725
Debbie1236 <debbieodhiambo91@gmail.com>
2024-09-08 19:10:02 +0300

    Update App.js

--- form-handling-react/src/App.js
+++ form-handling-react/src/App.js
@@ -1,1 +1,18 @@
+import React from 'react';
+import RegistrationForm from './components/RegistrationForm';
+import FormikForm from './components/FormikForm';
+
+function App() {
+  return (
+    <div>
+      <h1>Controlled Components Form</h1>
+      <RegistrationForm />
+      
+      <h1>Formik + Yup Form</h1>
+      <FormikForm />
+    </div>
+  );
+}
+
+export default App;
 
