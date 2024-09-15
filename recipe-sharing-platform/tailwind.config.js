b1d1e5cab6af970ca053d9b1c4242d1519c63ea4
Debbie1236 <debbieodhiambo91@gmail.com>
2024-09-15 10:02:33 +0300

    Create tailwind.config.js

---
+++ recipe-sharing-platform/tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  media: false,
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
