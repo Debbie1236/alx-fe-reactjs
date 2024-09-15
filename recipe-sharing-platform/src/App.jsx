bb193e9929c00775a7286a6c770a69b87341091a
Debbie1236 <debbieodhiambo91@gmail.com>
2024-09-15 10:05:18 +0300

    Create App.jsx

---
+++ recipe-sharing-platform/src/App.jsx
@@ -0,0 +1,17 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import RecipeDetail from "./components/RecipeDetail";
+import HomePage from "./components/Homepage";
+import AddRecipeForm from "./components/AddRecipeForm";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/recipe/:id" element={<RecipeDetail />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
